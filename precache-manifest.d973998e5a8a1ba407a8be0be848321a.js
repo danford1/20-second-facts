0f30f6db18edba1454b9721c059380773dafe0b5
danford1 <dan.john.ford@gmail.com>
2020-03-23 00:10:28 -0400

    Updates

---
+++ precache-manifest.d973998e5a8a1ba407a8be0be848321a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0a542b73e5c89c1dae99a7dfb9f563",
+    "url": "/20-second-facts/index.html"
+  },
+  {
+    "revision": "d27dd0e2fac240d3a65e",
+    "url": "/20-second-facts/static/css/main.8d23f103.chunk.css"
+  },
+  {
+    "revision": "8c0990de572543594e66",
+    "url": "/20-second-facts/static/js/2.120b5f88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/20-second-facts/static/js/2.120b5f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27dd0e2fac240d3a65e",
+    "url": "/20-second-facts/static/js/main.684e6d5c.chunk.js"
+  },
+  {
+    "revision": "686b191d65c5ffa1f8b7",
+    "url": "/20-second-facts/static/js/runtime-main.8092d78d.js"
+  }
+]);
